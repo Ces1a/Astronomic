827594534c0b164461c639f1acd8bc8d6d6cbcc4
Ces1a <cesiapajaroc@hotmail.com>
2024-11-05 18:40:41 -0500

    fadf

--- proyecto/src/Page/Explora.jsx
+++ proyecto/src/Page/Explora.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 const Explora = () => {
     return (
-        <div>
-            <h1>Página </h1>
-            <p>Bienvenido a la página. Aquí puedes encontrar información sobre diversos temas.</p>
-        </div>
+        <section>
+            <div>
+            <h1>seccion 50%, Astronomic picture of the day </h1>
+            </div>
+            <div>
+                <div>
+                    <h1>curiosidades</h1>
+                </div>
+                <div>
+                    <h1>iamegnes y descripciones</h1>
+                    <p>Un conjunto de imágenes del rover en Marte organizadas en una cuadrícula</p>
+                </div>
+                <div>
+                    <h1>Distant Worlds</h1>
+                    <p> Tarjetas que representan exoplanetas, cada una con un ícono de planeta, un nombre y un botón de "Más información".</p>
+                </div>
+            </div>
+        </section>
+    
     );
 };
 
