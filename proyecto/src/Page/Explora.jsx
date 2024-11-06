de63cdf1c5908138b258aa1e94425b58d48f3ac8
Ces1a <cesiapajaroc@hotmail.com>
2024-11-05 22:14:01 -0500

    new

--- proyecto/src/Page/Explora.jsx
+++ proyecto/src/Page/Explora.jsx
@@ -3,29 +3,16 @@ import axios from 'axios';
 
 const Explora = () => {
     const [marsImages, setMarsImages] = useState([]);  // Imágenes de Marte
-    const [asteroids, setAsteroids] = useState([]);    // Lista de asteroides cercanos
-    const [missionDesign, setMissionDesign] = useState(null);  // Diseño de misión de cuerpos pequeños
-
+    const [fireballData, setFireballData] = useState([]); // Datos de fireball
     const apiKey = "V1SUZ9nW8DllDfQrtADt7ugTtqucmDhqBjdlWt98";
 
     useEffect(() => {
-        // 1. Llamada para imágenes del rover en Marte (cámara fhaz)
+        // Llamada para obtener imágenes del rover en Marte (cámara fhaz)
         axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${apiKey}`)
-            .then(response => setMarsImages(response.data.photos.slice(0, 1))) // Limitar a 1 imágenes
+            .then(response => {
+                setMarsImages(response.data.photos.slice(0, 1));  // Limitar a 1 imagen
+            })
             .catch(error => console.error("Error al cargar imágenes de Marte:", error));
-
-        // 2. Llamada para recuperar una lista de asteroides cercanos en un rango de fechas
-        const startDate = "";
-        const endDate = "";
-        axios.get(`https://api.nasa.gov/neo/rest/v1/${apiKey}`)
-            .then(response => setAsteroids(Object.values(response.data.near_earth_objects).flat().slice(0, 2))) // Limitar a 5 asteroides
-            .catch(error => console.error("Error al cargar la lista de asteroides:", error));
-
-        // 3. Llamada para diseño de misión de cuerpos pequeños (Small-Body Mission Design)
-        axios.get(`https://api.nasa.gov/neo/rest/v1/neo/`)
-            .then(response => setMissionDesign(response.data))
-            .catch(error => console.error("Error al cargar el diseño de misión de cuerpos pequeños:", error));
-    }, []);
 
     return (
         <section className="explora-section">
@@ -38,33 +25,11 @@ const Explora = () => {
                             <img key={index} src={image.img_src} alt={`Mars Rover ${index}`} className="mars-image" />
                         ))}
                     </div>
-                ) : <p>Cargando imágenes de Marte...</p>}
-            </div>
-
-            {/* Sección de asteroides cercanos a la Tierra */}
-            <div className="asteroids-list">
-                <h1>Asteroides Cercanos a la Tierra</h1>
-                {asteroids.length > 0 ? (
-                    <ul>
-                        {asteroids.map((asteroid, index) => (
-                            <li key={index}>
-                                <p>Nombre: {asteroid.name}</p>
-                                <p>Distancia a la Tierra: {asteroid.close_approach_data[0].miss_distance.kilometers} km</p>
-                            </li>
-                        ))}
-                    </ul>
-                ) : <p>Cargando asteroides...</p>}
-            </div>
-
-            {/* Sección de diseño de misión de cuerpos pequeños */}
-            <div className="mission-design">
-                <h1>Diseño de Misión de Cuerpos Pequeños</h1>
-                {missionDesign ? (
-                    <pre>{JSON.stringify(missionDesign, null, 2)}</pre>
-                ) : <p>Cargando diseño de misión...</p>}
+                ) : (
+                    <p>Cargando imágenes de Marte...</p>
+                )}
             </div>
         </section>
-    
     );
 };
 
--- proyecto/src/Page/MisionVision.jsx
+++ proyecto/src/Page/MisionVision.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../../src/App.css"
+import cesiaImage from '../assets/img/Cesia.jpg'; 
+import wandaImage from '../assets/img/Wanda.jpg';
+import katheImage from '../assets/img/Katherine.jpg';
 
 const MisionVision = () => {
+    // Estado para almacenar la descripción de la persona seleccionada
+    const [selectedPerson, setSelectedPerson] = useState(null);
+
+    // Descripciones de las personas
+    const descriptions = {
+        cesia: "Cesia es una apasionada de la ciencia y el espacio, siempre está dispuesta a compartir su amor por el conocimiento.",
+        wanda: "Wanda es una ingeniera curiosa, siempre dispuesta a resolver problemas con creatividad y pasión.",
+        kathe: "Katherine es una líder motivadora, que se inspira en la exploración científica para educar y empoderar a otros."
+    };
+
+    // Función para manejar el clic en las imágenes
+    const handleImageClick = (person) => {
+        setSelectedPerson(descriptions[person]);
+    };
+
     return (
-        <div>
-            <h1>Página del Blog</h1>
-            <p>Bienvenido a la página del blog. Aquí puedes encontrar información sobre diversos temas.</p>
+        <div className="mision-vision-container">
+            <div className="mision-container">
+                <h1>Misión</h1>
+                <p>Brindar a los usuarios una experiencia educativa interactiva y 
+                    divertida, donde puedan explorar el espacio y aprender sobre el universo, 
+                    fomentando la curiosidad científica y el amor por la astronomía,
+                    a través de recursos accesibles y emocionantes.</p>
+            </div>
+            <div className="vision-container">
+                <h1>Visión</h1>
+                <p>Nuestra visión es brindar un contenido educativo 
+                    que inspire y motive a nuestros usuarios a explorar y 
+                    aprender más sobre el mundo que nos rodea.</p>
+            </div>
+            <div className="grupo-imagenes">
+                <h2>Equipo</h2>
+                <div className="imagenes-container">
+                    {/* Aquí las imágenes están configuradas para que, al hacer clic, muestren la descripción */}
+                    <img 
+                        src={cesiaImage} 
+                        alt="Cesia" 
+                        className="grupo-imagen" 
+                        onClick={() => handleImageClick('cesia')} 
+                    />  
+                    <img 
+                        src={wandaImage} 
+                        alt="Wanda" 
+                        className="grupo-imagen" 
+                        onClick={() => handleImageClick('wanda')} 
+                    />
+                    <img 
+                        src={katheImage} 
+                        alt="Katherine" 
+                        className="grupo-imagen" 
+                        onClick={() => handleImageClick('kathe')} 
+                    />
+                </div>
+                {/* Mostrar la descripción si hay una persona seleccionada */}
+                {selectedPerson && (
+                    <div className="descripcion-persona">
+                        <h3>Descripción:</h3>
+                        <p>{selectedPerson}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
